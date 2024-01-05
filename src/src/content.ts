c80e285aadd7cf358e77c0783e575bd0f17c218a
James Stow <james.stow@codewizards.co.uk>
2024-01-05 11:50:36 +0000

    tidy up

--- src/src/content.ts
+++ src/src/content.ts
@@ -1,23 +1,15 @@
 import * as models from "./models";
-export const MockContentSource : models.ContentSource = {
-    async getContent() : Promise<models.Gif[]> {
-        // const content : models.Content = {
-        //     gifs:[{
-        //         height:100,
-        //         lengthInMillis: 2000,
-        //         url: "test.gif",
-        //         width: 100
-        //     }]
-        // };
-try{
-        const result = await fetch("/content/gifs.json");
-        const json = await result.json()
-        const models = json as models.Gif[];
-        return models;
-}
-catch(e){
-    console.log(e);
-    return [];
-}
+export const ContentSource : models.ContentSource = {
+    async getContent() : Promise<models.Gif[]> {        
+        try{
+            const result = await fetch("/content/gifs.json");
+            const json = await result.json()
+            const models = json as models.Gif[];
+            return models;
+        }
+        catch(e){
+            console.log(e);
+            return [];
+        }
     }
 }
