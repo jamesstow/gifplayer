49c3c14289e9cdfd6d37cf8844de4601805ad782
James Stow <james.stow@codewizards.co.uk>
2024-01-05 11:50:51 +0000

    more tidy

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -6,21 +6,7 @@ import { GifPlayer } from './GifPlayer';
 function App() {
   return (
     <div className="gif-player">
-      <GifPlayer contentSource={content.MockContentSource} />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <GifPlayer contentSource={content.ContentSource} />
     </div>
   );
 }
